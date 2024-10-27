8aa09b020e532cc1930c498c25d4407070580abc
Muhammad Rezza <126452363+MuhammadRav@users.noreply.github.com>
2024-10-27 09:34:44 +0700

    Delete routes directory

--- routes/objekRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { products } = require("../models/objekModel");
-
-router.get("/products", (req, res) => {
-  res.json(products);
-});
-
-module.exports = router;
